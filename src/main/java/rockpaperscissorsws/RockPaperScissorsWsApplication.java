83447b6c0b36df33746370b76742f6d31a0f1091
burtmacklin16 <38384310+burtmacklin16@users.noreply.github.com>
2021-06-15 17:04:34 -0500

    initialized project with spring initializr

---
+++ src/main/java/rockpaperscissorsws/RockPaperScissorsWsApplication.java
@@ -0,0 +1,13 @@
+package rockpaperscissorsws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rockpaperscissorsws/RockPaperScissorsWsApplicationTests.java
@@ -0,0 +1,13 @@
+package rockpaperscissorsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
